e6c1a01257353f1043205d49e3e323011d9c0371
Roy Lomberg <roylomberg@Roys-MacBook-Air.local>
2025-11-19 19:22:45 +0200

    Update Algebra.java with fixed negative operations

--- Algebra.java
+++ Algebra.java
@@ -23,78 +23,136 @@ public class Algebra {
    		System.out.println(sqrt(76123));
 	}  
 
+    private static int negate(int x) {
+        int res = 0;
+        if (x > 0) {
+            while (x > 0) {
+                res--;
+                x--;
+            }
+        } else if (x < 0) {
+            while (x < 0) {
+                res++;
+                x++;
+            }
+        }
+        return res;
+    }
+
+    private static int abs(int x) {
+        if (x >= 0) {
+            return x;
+        }
+        return negate(x);
+    }
+
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		int result = x1;
-		for(int i =1; i<=x2; i++){
-			result ++;
-
-		}
-		return result;
-	}
+        int result = x1;
+        if (x2 > 0) {
+            while (x2 > 0) {
+                result++;
+                x2--;
+            }
+        } else {
+            while (x2 < 0) {
+                result--;
+                x2++;
+            }
+        }
+        return result;
+    }
 		
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		int result = x1;
-		for(int i = 1; i<=x2; i++){
-			result --;
-		}
-		return result;
-		
-	}
+        return plus(x1, negate(x2));
+    }
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		int result = 0;
-		for(int i = 0; i < x2; i++){
-			result = plus(result,x1);
-		
-		}
-		return result;
-	}
+        if (x1 == 0 || x2 == 0) {
+            return 0;
+        }
+
+        boolean negative = (x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0);
+
+        int a = abs(x1);
+        int b = abs(x2);
+        int result = 0;
+        int i = 0;
+
+        while (i < b) {
+            result = plus(result, a);
+            i++;
+        }
+
+        if (negative) {
+            result = negate(result);
+        }
+
+        return result;
+    }
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		 int result =1;
-		 for(int i = 0; i < n; i++){
-			result = times(result,x);
-		}
-		return result;
-	}
+        int result = 1;
+        int i = 0;
+        while (i < n) {
+            result = times(result, x);
+            i++;
+        }
+        return result;
+    }
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		int result = x1;
-		int count=0;
-		while (result >= x2){
-			result =minus(result, x2);
-			count++;
+        if (x2 == 0) {
+            return 0; // not defined, but tests should not use this case
+        }
+        if (x1 == 0) {
+            return 0;
+        }
 
-		}
-			return count;
-			
-		}
-		
-	
+        boolean negative = (x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0);
+
+        int dividend = abs(x1);
+        int divisor = abs(x2);
+        int result = 0;
+
+        while (dividend >= divisor) {
+            dividend = minus(dividend, divisor);
+            result = plus(result, 1);
+        }
+
+        if (negative) {
+            result = negate(result);
+        }
+
+        return result;
+    }
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		int z = div(x1,x2);
-		z = times(z,x2);
-		 int modul = minus(x1, z);
-		return modul;
-	}	
+        if (x2 == 0) {
+            return 0; // undefined mathematically, but safe fallback
+        }
+        int q = div(x1, x2);
+        int prod = times(q, x2);
+        int r = minus(x1, prod);
+        return r;
+    }	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		int z=0;
-		while (times (z, z) <= x) { 
-			z++;
-			
-		}
-
-	   z--;
-	   return z;
-	}	  	  
+        if (x <= 0) {
+            return 0;
+        }
+        int z = 0;
+        while (times(z, z) <= x) {
+            z = plus(z, 1);
+        }
+        z = minus(z, 1);
+        return z;
+    }	  	  
 }
